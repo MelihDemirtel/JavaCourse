e49e940bf4fb6bfd3f78590d0fc51ed3048945dc
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 15:57:03 +0300

    methods2 course is over

--- methods2/src/Main.java
+++ methods2/src/Main.java
@@ -1,6 +1,53 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String message = "Today is beautiful";
+        String newMessage = message.substring(0,2);
+        System.out.println(newMessage);
+
+        int number = sum(5,7);
+        System.out.println(number);
+
+        int total = sum2(1,2,3,4,5);
+        System.out.println(total);
+
+        String newMessage2 = giveCity();
+        System.out.println(newMessage2);
+
     }
+
+    public static void add(){
+        System.out.println("Comment added");
+
+    }
+
+    public static void delete(){
+        System.out.println("Comment deleted");
+
+
+    }
+
+    public static void update(){
+        System.out.println("Comment updated");
+
+
+    }
+
+    public static int sum(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public static int sum2(int... numbers){
+        int summing = 0;
+        for(int number: numbers){
+            summing += number;
+        }
+        return summing;
+    }
+
+    public static String giveCity(){
+        return "Ankara";
+    }
+
 }
