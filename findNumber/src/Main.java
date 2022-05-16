273b74d66e99e5425193495a7707a7171dc8dfdc
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 15:23:52 +0300

    findNumber course is over

--- findNumber/src/Main.java
+++ findNumber/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] numbers = new int[]{1, 21, 35, 46, 58, 62, 77, 8, 9};
+        int finding = 81;
+        boolean isThere = false;
+
+        for (int number : numbers) {
+            if (finding == number) {
+                isThere = true;
+                break;
+            }
+        }
+        if (isThere) {
+            System.out.println("Number is found in array list : " + finding);
+        } else {
+            System.out.println("Number is not found in array list : " + finding);
+        }
     }
 }
