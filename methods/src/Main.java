0ff63c5d87c0caf4efb435a392cd567bb280ae26
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 15:34:41 +0300

    methods course is over

--- methods/src/Main.java
+++ methods/src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        findingNumber();
+
+    }
+
+    public  static void findingNumber(){
+        int[] numbers = new int[]{1, 21, 35, 46, 58, 62, 77, 8, 9};
+        int finding = 81;
+        boolean isThere = false;
+
+        for (int number : numbers) {
+            if (finding == number) {
+                isThere = true;
+                break;
+            }
+        }
+        if (isThere) {
+            giveMessage("Number is found in array list : " + finding);
+        } else {
+            giveMessage("Number is not found in array list : " + finding);
+        }
+    }
+    public static void giveMessage(String message){
+        System.out.println(message);
     }
 }
