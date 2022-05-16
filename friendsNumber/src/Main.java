42366209d30c8b3f09ce82842ed1e5cd4231d955
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 15:09:41 +0300

    friendsNumber course is over

--- friendsNumber/src/Main.java
+++ friendsNumber/src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //220-284
+        int number1 = 220;
+        int number2 = 284;
+        int total1 = 0;
+        int total2 = 0;
+
+        for (int i = 1; i < number1; i++) {
+            if (number1 % i == 0) {
+                total1 = total1 + i;
+            }
+        }
+        for (int j = 1; j < number2; j++) {
+            if (number2 % j == 0) {
+                total2 = total2 + j;
+            }
+        }
+        if (total1 == number2 && total2 == number1) {
+            System.out.println("Numbers are Friends");
+        } else {
+            System.out.println("Numbers are not Friends");
+        }
     }
 }
