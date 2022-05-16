c62b03321818da56356dd724013c55af2bdc8c68
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 10:31:13 +0300

    loopDemo course is over

--- loopDemo/src/Main.java
+++ loopDemo/src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //For
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("For Loop is over");
+
+        //While
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While Loop is over");
+
+        //Do-While
+        int j = 1;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 10);
+        System.out.println("Do-While Loop is over");
     }
 }
