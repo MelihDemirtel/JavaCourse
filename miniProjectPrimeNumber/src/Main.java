7efd085381326ff6fa048862f8bea761471fd80c
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 14:12:03 +0300

    miniProjectPrimeNumber is over

--- miniProjectPrimeNumber/src/Main.java
+++ miniProjectPrimeNumber/src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int number = 1;
+        int remainder = number % 2;
+        System.out.println("Remainder : " + remainder);
+        boolean isPrime = true;
+
+        if(number == 1){
+            System.out.println("Number is not Prime");
+            return;
+        }
+        if(number < 1){
+            System.out.println("Invalid Number");
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
+
+        }
+
+        if (isPrime) {
+            System.out.println("Number is Prime");
+        } else {
+            System.out.println("Number is not Prime");
+        }
     }
 }
