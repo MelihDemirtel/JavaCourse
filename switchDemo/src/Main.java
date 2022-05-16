b6b693dfdd73ec13ebaf6a8c889f6b996da92756
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 10:03:09 +0300

    switchDemo course is over

--- switchDemo/src/Main.java
+++ switchDemo/src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char grade = 'A';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("You Passed the Exam Your Score A");
+                break;
+            case 'B':
+                System.out.println("You Passed the Exam Your Score B");
+                break;
+            case 'C':
+                System.out.println("You Passed the Exam Your Score C");
+                break;
+            case 'D':
+                System.out.println("You Passed the Exam Your Score D");
+                break;
+            case 'F':
+                System.out.println("You Failed the Exam Your Score F");
+                break;
+            default:
+                System.out.println("Invalid Grade Entry");
+        }
     }
 }
