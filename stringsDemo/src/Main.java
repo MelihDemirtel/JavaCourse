98c0167772bb1ce4710c059e6ad4b4d71c57f239
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 13:33:28 +0300

    stringsDemo first part is over

--- stringsDemo/src/Main.java
+++ stringsDemo/src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String message = "Today is beautiful.";
+        System.out.println(message);
+
+        System.out.println("Number of Character : " + message.length());
+
+        System.out.println("Fifth character : " + message.charAt(4));
+
+        System.out.println(message.concat(" Amazing!"));
+
+        System.out.println(message.startsWith("T"));
+
+        System.out.println(message.endsWith("*"));
+
+        char[] array = new char[5];
+        message.getChars(0,5,array,0);
+        System.out.println(array);
+
+        System.out.println(message.indexOf('t'));
+
+        System.out.println(message.lastIndexOf("t"));
+
     }
 }
