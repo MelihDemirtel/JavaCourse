c45236c6f85ce1648b661269c3787c81809c58dc
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 14:30:38 +0300

    vowels course is over

--- vowels/src/Main.java
+++ vowels/src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char letter = 'o';
+
+        switch (letter) {
+            case 'a':
+            case 'ı':
+            case 'o':
+            case 'u':
+                System.out.println("Character is bold");
+                break;
+            case 'e':
+            case 'i':
+            case 'ö':
+            case 'ü':
+                System.out.println("Character is tiny");
+                break;
+            default:
+                System.out.println("Invalid Grade Entry");
+
+        }
     }
 }
