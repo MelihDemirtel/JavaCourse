e2d19470b4a5a6d1f94e4587a5ae54d4179917ba
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 08:44:44 +0300

    The topic of variables is over.

--- variables/src/Main.java
+++ variables/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int numberOfStudent = 10;
+        String message = "Number of Student : ";
+
+        //Only use string
+        System.out.println("Number of Student : 10");
+
+        //Use string + variable
+        System.out.println("Number of Student : " + numberOfStudent);
+
+        //Use variable + string
+        System.out.println(message + "10");
+
+        //Use variable + variable
+        System.out.println(message + numberOfStudent);
     }
 }
