185febc8c850571ab5dc40f6949508ccb894bd24
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-16 09:46:35 +0300

    recapDemo1 course is over

--- recapDemo1/src/Main.java
+++ recapDemo1/src/Main.java
@@ -1,6 +1,45 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int numberOne = 25;
+        int numberTwo = 2;
+        int numberThree = 26;
+        int largestNumber;
+        int largestNumber2 = numberOne;
+
+        if(numberOne > numberTwo) {
+            largestNumber = numberOne;
+            if (numberOne > numberThree) {
+                largestNumber = numberOne;
+                System.out.println("Largest Number is : " + largestNumber);
+            }
+        }
+        if(numberTwo > numberOne){
+            largestNumber = numberTwo;
+            if(numberTwo > numberThree){
+                largestNumber = numberTwo;
+                System.out.println("Largest Number is : "+largestNumber);
+            }
+        }
+        if(numberThree > numberOne){
+            largestNumber = numberThree;
+            if(numberThree > numberTwo){
+                largestNumber = numberThree;
+                System.out.println("Largest Number is : "+largestNumber);
+            }
+        }
+
+        //Another Example
+
+        if(largestNumber2 < numberTwo){
+            largestNumber2 = numberTwo;
+        }
+        if(largestNumber2 < numberThree){
+            largestNumber2 = numberThree;
+        }
+
+        System.out.println("Largest number is : "+largestNumber2);
+
     }
 }
