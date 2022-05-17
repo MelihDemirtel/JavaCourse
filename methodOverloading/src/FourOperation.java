2e89ea319371a04d58d72e48f8e9bdaec424fcfc
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-17 10:47:42 +0300

    methodOverloading course is over

--- methodOverloading/src/FourOperation.java
+++ methodOverloading/src/FourOperation.java
@@ -1,2 +1,18 @@
 public class FourOperation {
+
+    public int Summing(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public int Summing(int number1, int number2, int number3){
+        return number1 + number2 + number3;
+    }
+
+    public int Summing(int... numbers){
+        int total = 0;
+        for(int number: numbers){
+            total += number;
+        }
+        return total;
+    }
 }
--- methodOverloading/src/Main.java
+++ methodOverloading/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        FourOperation fourOperation = new FourOperation();
+        int result = fourOperation.Summing(2,4);
+        System.out.println(result);
+
+        int result2 = fourOperation.Summing(1,2,3);
+        System.out.println(result2);
+
+        int result3 = fourOperation.Summing(1,2,3,4,5);
+        System.out.println(result3);
     }
 }
