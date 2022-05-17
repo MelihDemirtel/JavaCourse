b49ebc7dc7a3de0769cbbfeff4f5699fdcbe3731
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-17 13:49:17 +0300

    consoleLogger java class created in polymorphismDemo

--- polymorphismDemo/src/Main.java
+++ polymorphismDemo/src/Main.java
@@ -2,9 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EmailLogger emailLogger = new EmailLogger();
-        emailLogger.Log("Log Message");
+        BaseLogger[] loggers = new BaseLogger[]{new FileLogger(), new EmailLogger(), new DatabaseLogger()};
 
-
+        for(BaseLogger logger: loggers){
+            logger.Log("Log Message");
+        }
     }
 }
