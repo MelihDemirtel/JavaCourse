0988aa151a5edfc0f48491ddc37dac21414bea2b
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-17 09:29:41 +0300

    recapDemoClasses course is over

---
+++ recapDemo_Classes/src/FourOperation.java
@@ -0,0 +1,14 @@
+public class FourOperation {
+    public int Summing(int number1, int number2){
+        return number1 + number2;
+    }
+    public int Extraction(int number1, int number2){
+        return number1 - number2;
+    }
+    public int Division(int number1, int number2){
+        return number1 / number2;
+    }
+    public int multiplication(int number1, int number2){
+        return number1 * number2;
+    }
+}
--- recapDemo_Classes/src/Main.java
+++ recapDemo_Classes/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        FourOperation fourOperation = new FourOperation();
+        int result = fourOperation.Summing(3,4);
+        System.out.println(result);
     }
 }
