2b98c8d7b66459b110b4f3a409ada1b04a73f56f
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-17 10:34:50 +0300

    classesWithAttributes course is over

--- classesWithAttirubutes/src/Main.java
+++ classesWithAttirubutes/src/Main.java
@@ -2,14 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Product product = new Product(1,"Laptop","Asus Laptop",5000,3,"Blue");
+
         Product product = new Product();
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Asus Laptop");
+        product.setPrice(5000);
+        product.setStockAmount(3);
+        product.setColor("Blue");
 
-        product.name = "Laptop";
-        product.id = 1;
-        product.description = "Asus Laptop";
-        product.price = 5000;
-        product.stockAmount = 3;
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+        System.out.println(product.getCode());
 
-        System.out.println(product.name);
     }
 }
--- classesWithAttirubutes/src/ProductManager.java
+++ classesWithAttirubutes/src/ProductManager.java
@@ -1,2 +1,8 @@
 public class ProductManager {
+
+    public void Add(Product product){
+        //JDBC
+        System.out.println("Product Added = "+product.getName());
+    }
+
 }
