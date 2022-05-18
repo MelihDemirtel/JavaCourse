186efe8c1e610fefe9bc6d39c9bbef6459b912a8
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-18 09:44:39 +0300

    interfaces course is over

--- interfaces/src/CustomerManager.java
+++ interfaces/src/CustomerManager.java
@@ -1,2 +1,13 @@
 public class CustomerManager {
+
+    private ICustomerDal customerDal;
+
+    public CustomerManager(ICustomerDal customerDal){
+        this.customerDal = customerDal;
+    }
+
+    public void add(){
+        //Business Codes
+        customerDal.add();
+    }
 }
