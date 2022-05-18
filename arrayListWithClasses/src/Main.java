93cb37c3f6448f073d0dd316fd80dd1f31bb0121
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-18 14:40:04 +0300

    arrayListWithClasses course is over

--- arrayListWithClasses/src/Main.java
+++ arrayListWithClasses/src/Main.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+
+        Customer melih = new Customer(1,"Melih","Han");
+        customers.add(melih);
+        customers.add(new Customer(2,"Can","Kan"));
+        customers.add(new Customer(3,"Demir","Tan"));
+
+        customers.remove(melih);
+        for(Customer customer:customers){
+            System.out.println(customer.firstName);
+        }
     }
 }
