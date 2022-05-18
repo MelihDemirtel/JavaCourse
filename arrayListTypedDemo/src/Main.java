f072c822aba30943f58da0196971a53d984535ae
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-18 14:28:48 +0300

    arrayListTypedDemo course is over

--- arrayListTypedDemo/src/Main.java
+++ arrayListTypedDemo/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<String> cities = new ArrayList<String>();
+        cities.add("Ankara");
+        cities.add("İstanbul");
+        cities.add("Bursa");
+        cities.add("Konya");
+        cities.add("Adana");
+
+        cities.remove("İstanbul");
+
+        Collections.sort(cities);
+
+        for (String city:cities){
+            System.out.println(city);
+        }
+
+
     }
 }
