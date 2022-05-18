1673610f4b91ab1828142ff8476eb74b8401bcdd
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-18 13:33:29 +0300

    math package created in packagesDemo

--- packagesDemo/src/Main.java
+++ packagesDemo/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entry Your Name : ");
+        String name = scanner.nextLine();
+        System.out.println("Hello "+name);
     }
 }
