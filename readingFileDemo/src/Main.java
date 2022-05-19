b00ee9286d3dd6deb8d6a62b4bfd2b20b52fefc8
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-19 13:35:14 +0300

    readingFileDemo course is over

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -1,10 +1,31 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BufferedReader reader = new BufferedReader(new FileReader());
+        BufferedReader reader = null;
+        int total = 0;
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\ext02d47194\\Desktop\\JavaCourse\\readingFileDemo\\src\\numbers.txt"));
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                total += Integer.valueOf(line);
+            }
+            System.out.println("Total : "+total);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
