2b1278cf00a339999fb209002d31eb43bc3bade6
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-19 13:16:11 +0300

    numbers.txt java file created in readingFileDemo

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        BufferedReader reader = new BufferedReader(new FileReader());
     }
 }
