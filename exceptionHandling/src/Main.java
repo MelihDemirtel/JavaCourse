e2d7bf4dcb88aae10e11bea45e3b088eb4324cb1
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-19 13:13:25 +0300

    exceptionHandling course is over

--- exceptionHandling/src/Main.java
+++ exceptionHandling/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try{
+            int[] numbers = new int[]{1,2,3};
+            System.out.println(numbers[5]);
+        }catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println(exception);
+        }catch (StringIndexOutOfBoundsException exception){
+            System.out.println(exception);
+        }catch (Exception exception){
+            System.out.println("Logged : "+exception);
+        }
+        finally {
+            System.out.println("This block always runs");
+        }
     }
 }
