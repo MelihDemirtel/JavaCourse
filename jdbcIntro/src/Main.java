c990ede563d79b04e9c0aab5477ce834657c2b7a
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-23 11:51:07 +0300

    dbHelper java class created in jdbcIntro

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,6 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    static String userName="root";
+    static String password="12345";
+    static String dbUrl="jdbc:mysql://localhost:3306/world";
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(dbUrl,userName,password);
+            System.out.println("Connection OK");
+        }catch (SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally {
+            connection.close();
+        }
     }
 }
