e8c6fae2a90ea7b80e2ce3d644ae81701b2ee998
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-23 08:19:38 +0300

    files file created in workingWithFiles

--- workingWithFiles/src/Main.java
+++ workingWithFiles/src/Main.java
@@ -1,6 +1,9 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        File file = new File("C:\\Users\\ext02d47194\\Desktop\\JavaCourse\\files");
     }
 }
