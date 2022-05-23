e3a7116e0e1814fb4e866ee82ae3f90d81c85db7
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-23 09:46:36 +0300

    productDal java class created in repositoryDemo

--- repositoryDemo/src/CustomerDal.java
+++ repositoryDemo/src/CustomerDal.java
@@ -1,3 +1,17 @@
-public class CustomerDal implements IEntityRepository{
+public class CustomerDal implements IEntityRepository<Customer>{
 
+    @Override
+    public void add(Customer entity) {
+
+    }
+
+    @Override
+    public void remove(Customer entity) {
+
+    }
+
+    @Override
+    public void update(Customer entity) {
+
+    }
 }
