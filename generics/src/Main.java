8b03e87d59d8626b3164247580d14d1c1402d885
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-23 09:31:42 +0300

    myList java class in created generics

--- generics/src/Main.java
+++ generics/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<String> cities = new ArrayList<String>();
+        cities.add("Ankara");
+        cities.add("İstanbul");
+        cities.add("İzmir");
+
     }
 }
